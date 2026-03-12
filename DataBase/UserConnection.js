75c08cb5e2f26de4cf4b70f823c5104db6021e37
Mahmoud khaled <109881136+7oda234@users.noreply.github.com>
2026-03-12 13:34:21 +0200

    add user connection

---
+++ DataBase/UserConnection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "",
+database: "lawlink",
+port: 3307,
+});
+con.connect(function(err) {
+if (err) throw err;
+con.query("SELECT * FROM users", function (err, result, fields) {
+if (err) throw err;
+console.log(result);
+});
+});
